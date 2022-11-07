42011aa04f5d368c9c0fe04ecdd719883f89a762
Gizelle Paisley <shenorap@gmail.com>
2022-11-06 21:32:15 -0500

    Exercise 3 completed

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-
 document.getElementById("search-for").addEventListener("click", () => {
+  let value = document.getElementById("nonmal").value;
+  if(value === null || value.trim() === ""){
+    FetchSuperHeroData();
+    return;
+    }
+    FetchHero(sanitizeInput(value));
+});
+
+
+function sanitizeInput(str) {
+  str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, " ");
+  return str.trim();
+}
+
+function FetchSuperHeroData() {
   fetch("http://localhost/info2180-lab4/superheroes.php")
     .then((result) => result.text())
-    .then((data) => alert(data))
+    .then((data) => updatediv(data))
     .catch((err) => alert(err));
-});
+}
+
+function FetchHero(heroName) {
+  fetch(`http://localhost/info2180-lab4/superheroes.php?query=${heroName}`)
+    .then((result) => result.text())
+    .then((data) => updatediv(data))
+    .catch((err) => alert(err));
+}
+
+function updatediv(data) {
+  document.getElementById("result").innerHTML = data;
+}
